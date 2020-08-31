9d90f7e839762ca11e9575b94c0c99f6e8645b18
Lucian Neghina <lucian.neghina@esolutions.ro>
2020-08-31 22:33:56 +0300

    moved spark-utils to external project https://github.com/eSolutionsGrup/spark-app-base

--- spark/src/main/scala/ro/esolutions/spark/JobRunnable.scala
+++
@@ -1,7 +0,0 @@
-package ro.esolutions.spark
-
-import org.apache.spark.sql.SparkSession
-
-trait JobRunnable[Context, Result] {
-  def run(implicit spark: SparkSession, context: Context): Result
-}
--- spark/src/main/scala/ro/esolutions/spark/io/Buckets.scala
+++
@@ -1,3 +0,0 @@
-package ro.esolutions.spark.io
-
-case class Buckets(number: Int, bucketColumns: Seq[String], sortByColumns: Seq[String] = Seq())
